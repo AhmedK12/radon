3847b7e4ed1b52ea683e24427d9554d4e39d83d2
Kamaran Ahmed <ahmedkamaran893@gmail.com>
2022-06-02 22:58:30 +0530

    nodejs query-prams

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const myHelper = require('../util/helper')
 const underscore = require('underscore')
+const moment = require('moment');
+const lodash = require('lodash')
 
 const router = express.Router();
 
@@ -13,8 +15,45 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+const chunk = ()=>{
+    moment.locale('en'); // sets words language (optional if current locale is to be used)
+    let months = moment.months()    
+    console.log(lodash.chunk(months,4))
+}
+
+const tail = ()=>{
+    let oddnum = [1,3,5,7,9,11,13,15,17,19]
+    console.log(lodash.tail(oddnum))
+}
+
+let a = [
+    [1,3,4,567,43,4567,3,2,34,345,212],
+    [21,23,34,43,345,45678,234,23],
+    [1,2,3,4,5,6,7,8,9,23,34,43,23432],
+    [23,4,3,2,1,34,43,345],
+    [1,2,3,4,21,43,34,345,5666,898]
+]
+
+const union = ()=>{
+    
+    
+    console.log(lodash.union(a[0],a[1],a[2],a[3],a[4]))
+}
+
+const fromPairs = ()=>{
+    
+    let pairs = [['Kamaran Ahmed','Coder'],['King Khan','SRK'],['Mr.Cricketer','Michael Hussey'],['Mr.Perfectionist','Aamir Khan'],['Mr.Ipl','Suresh Raina'],['Run Machine','Virat Kohli']]
+    
+    console.log(lodash.fromPairs(pairs))
+}
+
+
 router.get('/hello', function (req, res) {
-   
+
+    // chunk()
+    // tail()
+    // union()
+    fromPairs()
     res.send('Hello there!')
 });
 
